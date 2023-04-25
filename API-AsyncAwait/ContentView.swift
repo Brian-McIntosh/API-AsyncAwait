f6152278dd118afa885c55cdccf14026b515cfa6
Brian McIntosh <brian@mixinteractive.com>
2023-04-25 07:11:47 -0400

    Added Model, ViewModel, and getAPI Call

--- API-AsyncAwait/ContentView.swift
+++ API-AsyncAwait/ContentView.swift
@@ -7,15 +7,74 @@
 
 import SwiftUI
 
+struct Quote: Codable {
+    //var id = UUID()
+    var quote: String
+    var author: String
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var quotes: [Quote] = []
+    
+    let dummyData: [Quote] = [
+        Quote(quote: "Give me my baby!", author: "Skylar"),
+        Quote(quote: "I am the danger.", author: "Walt"),
+        Quote(quote: "Yeah, science!", author: "Jesse")
+    ]
+}
+
 struct ContentView: View {
+    
+    @StateObject private var vm = ContentViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            
+            // ===>>> DID NOT NEED TO ADD A FOREACH INSIDE A LIST!!!
+            //List {
+                //ForEach(vm.dummyData, id: \.quote_id) { quote in
+            
+            List(vm.quotes, id: \.quote) { quote in
+                VStack(alignment: .leading) {
+                    
+                    Text(quote.quote)
+                        .font(.system(size: 26, weight: .bold))
+                        .foregroundColor(Color("skyBlue"))
+                    
+                    Text(quote.author)
+                        .font(.system(size: 22, weight: .bold))
+                        .foregroundColor(.gray)
+                        .foregroundColor(.secondary)
+                }
+                .padding()
+            }
+            .navigationTitle("Quotes")
+            .task {
+                await getData()
+            }
         }
-        .padding()
+    }
+    
+    func getData() async {
+
+        // 1. create the url
+        guard let url = URL(string: "https://api.breakingbadquotes.xyz/v1/quotes/5") else {
+            print("bad url")
+            return
+        }
+        
+        // 2. fetch data from that url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // decode that data
+            if let decodedData = try? JSONDecoder().decode([Quote].self, from: data) {
+                vm.quotes = decodedData
+            }
+        } catch {
+            print("invalid data")
+        }
     }
 }
 
